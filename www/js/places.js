4bb487727428c506406abc9ce20622ff850cb0aa
unknown <alexanderyordanov96@gmail.com>
2015-04-25 16:23:50 +0300

    trying to fix map displaying issue

--- www/js/places.js
+++ www/js/places.js
@@ -1,31 +1,32 @@
-function initialize() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-        zoom: 8,
-        center: latlng,
+$(document).on('pageshow', '#places-page', function (event) {
+    max_height();
+        navigator.geolocation.getCurrentPosition(onSuccess, onError,{'enableHighAccuracy':true,'timeout':20000});
+});
+
+function max_height() {
+    var header = $.mobile.activePage.find("div[data-role='header']:visible");
+    var footer = $.mobile.activePage.find("div[data-role='footer']:visible");
+    var content = $.mobile.activePage.find("div[data-role='content']:visible:visible");
+    var viewport_height = $(window).height();
+    
+    var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
+    if((content.outerHeight() - header.outerHeight() - footer.outerHeight()) <= viewport_height) {
+        content_height -= (content.outerHeight() - content.height());
+    } 
+    $.mobile.activePage.find('[data-role="content"]').height(content_height);
+}
+
+
+function onSuccess(position) {       
+    var minZoomLevel = 15;
+    
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: minZoomLevel,
+        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-            myOptions);
-}
-google.maps.event.addDomListener(window, "load", initialize);
-
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+    });    
 }
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+function onError() {
+    alert('Error');
+}
